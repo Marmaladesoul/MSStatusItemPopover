b2e4b8cf9a8e74b1f0bd5d8e06dd5c14494764f5
Jeff Hanbury <jeff@marmaladesoul.com>
2014-08-22 21:32:23 +1200

    Add PopoverContentView.

---
+++ MSStatusItemPopover/PopoverContentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverContentViewController.swift
+//  MSStatusItemPopover
+//
+//  Created by Jeff Hanbury on 22/08/2014.
+//  Copyright (c) 2014 Marmaladesoul. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverContentViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
